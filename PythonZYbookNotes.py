7305757aff904cb25c84e36cfda849969f359a30
jcL2code <47303945+jcL2code@users.noreply.github.com>
2019-02-03 18:22:42 -0500

    Add files via upload

---
+++ PythonZYbookNotes.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print('Hello Dr. Baldo!,\n')
+
+print('Thank you for your help this week! As I continue to work on my zyBook sections \nI will post ones that i find interesting below:')
+print()
+print()
+print('I created the USA flag with what I learned from section 6.1:')
+print()
+# my interpretation of acitivity 6.1.1 - Printing a face to printing the US Flag
+def print_flag():
+    usa1 = '='
+    usa2 = '*'
+    print(usa2 * 10,usa1 * 35)
+    print(usa2 * 10,usa1 * 35)
+    print(usa2 * 10,usa1 * 35)
+    print(usa2 * 10,usa1 * 35)
+    print(usa2 * 10,usa1 * 35)
+    print(usa1*46)
+    print(usa1*46)
+    print(usa1*46)
+    print(usa1*46)
+    print(usa1*46)
+    return
+print_flag()
+
